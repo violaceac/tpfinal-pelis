07b173f8f4ffd6c900283f4937c2e25f351b0784
Violeta Contreras <viol.contreras@gmail.com>
2025-07-30 18:35:42 -0300

    primer router

---
+++ src/pages/PelisFavoritas.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PelisFavoritas() {
+  return (
+    <div>PelisFavoritas</div>
+  )
+}
+
+export default PelisFavoritas
---
+++ src/pages/UltimosLanzamientos.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function UltimosLanzamientos() {
+  return (
+    <div>UltimosLanzamientos</div>
+  )
+}
+
+export default UltimosLanzamientos
