0b1257f38bae4a2d240db05a59f879de009f8e16
Noelia2024 <noelialucerodev@gmail.com>
2025-08-14 23:43:59 -0300

    vista de ultimos lanzamientos

--- src/pages/UltimosLanzamientos.jsx
+++ src/pages/UltimosLanzamientos.jsx
@@ -1,7 +1,47 @@
-import React from 'react'
+import { useEffect } from "react";
+import usePeliculas from "../hooks/usePeliculas";
 
-export default function UltimosLanzamientos() {
-  return (
-    <div>UltimosLanzamientos</div>
+export default function ultimosLanzamientos() {
+  const {
+    obtenerUltimosLanzamientos,
+    pelis,
+    pagina,
+    paginaAnterior,
+    paginaSiguiente,
+    totalPaginas,
+  } = usePeliculas();
+
+  useEffect (()=>{
+    obtenerUltimosLanzamientos();
+  },[pagina]);
+
+
+  return (<>
+    <h2>Ultimos lanzamientos</h2>
+    <div>
+      {pelis.map((peli) => (
+      <div key={peli.id} >
+        <img
+          src={`https://image.tmdb.org/t/p/w300${peli.poster_path}`}
+          alt={peli.title}
+      
+        />
+        <h3>{peli.title}</h3>
+      </div>
+    )
+    )}
+    </div>
+    <div>
+      <h2>Probando Paginación</h2>
+      <div>
+        <button onClick={paginaAnterior} disabled={pagina < 1}>Anterior</button>
+          <span>Página {pagina} de {totalPaginas}</span>
+          <button onClick={paginaSiguiente} disabled={pagina === totalPaginas}>Siguiente</button>
+      </div>
+    </div>
+  </>
   )
 }
+
+
+ 
