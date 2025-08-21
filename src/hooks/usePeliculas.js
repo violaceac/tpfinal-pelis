9397cf3754cc356cb6702522ba3bd5cfef341c03
Noelia2024 <noelialucerodev@gmail.com>
2025-08-20 22:27:12 -0300

    vista detallepelicula

--- src/hooks/usePeliculas.js
+++ src/hooks/usePeliculas.js
@@ -6,41 +6,73 @@ export default function usePeliculas() {
 
     const [pelis, setPelis] = useState([])
     const [pagina, setPagina] = useState(1)
-    const [totalPaginas, setTotalPaginas] = useState(1);
+    const [totalPaginas, setTotalPaginas] = useState(1)
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(null)
+    const [peliculaDetalle, setPeliculaDetalle] = useState(null)
     
-
-    
-    //funcion para obtener peliculas populares
-    async function obtenerMasPopulares(){
+     
+    //funcion para obtener peliculas 
+    async function obtenerPeliculas(tipo){
       const apiKey = import.meta.env.VITE_API_KEY_PELICULAS;
-      const url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=es-ES&page=${pagina}`;
+      const url = `https://api.themoviedb.org/3/movie/${tipo}?api_key=${apiKey}&language=es-ES&page=${pagina}`;
       
       try {
             const {data} = await axios.get(url)
             console.log (data)
-            setPelis(data.results)
-            setTotalPaginas(data.total_pages)
+            setPelis(data.results);
+            setTotalPaginas(data.total_pages);
+            setLoading(true);
+            setError(null);
           } catch (error) {
-            console.error("Error al obtener peliculas")
+            setError("Error al obtener peliculas");
+            setLoading(false)
           }
+
+      
+    }
+    
+    
+     // Paginacion
+    function paginaAnterior() {
+      if (pagina > 1) setPagina(pagina - 1);
     }
       
-        function paginaAnterior() {
-          if (pagina > 1) setPagina(pagina - 1);
-        }
-      
-        function paginaSiguiente() {
-          if (pagina < totalPaginas) setPagina(pagina + 1);
-        }
+    function paginaSiguiente() {
+      if (pagina < totalPaginas) setPagina(pagina + 1);
+    }  
     
-        const data = {
-          pelis,
-          pagina,
-          totalPaginas,
-          obtenerMasPopulares,
-          paginaAnterior,
-          paginaSiguiente
-        }
+    //detalle pelicula
+    async function obtenerDetallePelicula(id){
+      setLoading(true);
+       try{
+        const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}&language=es-ES&append_to_response=videos`;
+        const { data } = await axios.get(url);
+        setPeliculaDetalle(data);
+        setError(null);
+      } catch (err) {
+        console.error(err);
+        setError ("Error al obtener el detalle de la pelicula");
+      } finally {
+       setLoading(false);
+      }
+   }
+      
+  
+
+    
+    const data = {
+      pelis,
+      pagina,
+      totalPaginas,
+      loading,
+      error,
+      obtenerPeliculas,
+      obtenerDetallePelicula,
+      paginaAnterior,
+      paginaSiguiente
+    
+    }
   
     return data
   }
---
+++ src/pages/DetallePelicula.jsx
@@ -0,0 +1,116 @@
+import { useEffect } from 'react' 
+import { useParams } from "react-router"
+import usePeliculas from '../hooks/usePeliculas'
+
+
+import ReactPlayer from "react-player";
+import {
+  MediaController,
+  MediaControlBar,
+  MediaTimeRange,
+  MediaTimeDisplay,
+  MediaVolumeRange,
+  MediaPlaybackRateButton,
+  MediaPlayButton,
+  MediaSeekBackwardButton,
+  MediaSeekForwardButton,
+  MediaMuteButton,
+  MediaFullscreenButton,
+} from "media-chrome/react";
+
+
+export default function DetallePelicula() {
+ 
+ const {id} = useParams();
+ const {
+   peliculaDetalle,
+   obtenerDetallePelicula,
+   loading,
+   error,
+  } = usePeliculas();
+
+
+useEffect (()=>{
+  console.log('ID de la película:', id);
+  obtenerDetallePelicula(id);
+  console.log("hols")
+  }
+,[id])
+
+//si esta cargando , muestra cargando . 17/8
+if(loading) {
+   return <div>Cargando...</div> 
+}
+
+// Paso 2: Muestra el estado de error
+if (error) {
+   return <div>Error: {error}</div>;
+}
+
+// Si no se encontró la película después de la carga, muestra un mensaje. 17/8 // 
+//  if (!peliculaDetalle) {
+//   return <div>No se pudo encontrar la película.</div>;
+// }
+
+const trailer = peliculaDetalle.videos?.results?.find(
+   (v) => v.type === "Trailer" && v.site === "YouTube"
+  );
+
+const trailerUrl = trailer ? `https://www.youtube.com/watch?v=${trailer.key}` : null;
+
+return (
+   <div>
+    <div style={{ maxWidth: "900px", margin: "0 auto" }}>
+      <h2>{peliculaDetalle.title}</h2>
+
+
+  {/* poster */}
+    <img
+      src={`https://image.tmdb.org/t/p/w500${peliculaDetalle.poster_path}`}
+      alt={peliculaDetalle.title}
+      style={{ width: "300px", borderRadius: "8px" }}
+    />
+
+
+{/* Sinopsis */}
+   <p>{peliculaDetalle.overview}</p>
+
+
+{/* Trailer */}
+  {trailerUrl ? (
+    <MediaController style={{ width: "100%", aspectRatio: "16/9", marginTop: "20px" }}>
+    <ReactPlayer
+      slot="media"
+      url={trailerUrl}
+      playing
+      controls={false}
+      width="100%"
+      height="100%"
+       style={{ "--controls": "none" }}
+    />
+    <MediaControlBar>
+    <MediaPlayButton />
+    <MediaSeekBackwardButton seekOffset={10} />
+    <MediaSeekForwardButton seekOffset={10} />
+    <MediaTimeRange />
+    <MediaTimeDisplay showDuration />
+    <MediaMuteButton />
+    <MediaVolumeRange />
+    <MediaPlaybackRateButton />
+    <MediaFullscreenButton />
+    </MediaControlBar>
+    </MediaController>
+ ) : (
+  <p>No hay trailer disponible</p>
+  )}
+  </div>
+</div>
+);
+} 
+
+
+  
+
+
+
+
