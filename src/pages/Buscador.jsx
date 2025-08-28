b7e1f1b97fb41f484972d0d46f7dbb0bfcffa4c7
Violeta Contreras <viol.contreras@gmail.com>
2025-08-28 16:33:31 -0300

    estilos buscador listos

--- src/pages/Buscador.jsx
+++ src/pages/Buscador.jsx
@@ -1,11 +1,16 @@
-import { Card } from "@mui/material";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import usePeliculas from "../hooks/usePeliculas";
+import { FavoriteContext } from '../context/FavoriteContext';
+
+import { Button, Card, CardContent, CardMedia, IconButton, Input, Typography } from "@mui/material";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 
 
 function mostrarBusqueda() {
   const { pelis, loading, error, buscarPeliculas } = usePeliculas();
   const [textoBusqueda, setTextoBusqueda] = useState("");
+  const { isFavorite, handleFavoriteClick } = useContext(FavoriteContext);
 
 
   // cada vez que cambia el search, hago la búsqueda
@@ -21,15 +26,35 @@ function mostrarBusqueda() {
 
   return (
     <div>
-      <h1>Buscador de Películas</h1>
-       <div className="search-container">
-        <input
+      <Typography
+      sx={{
+        fontSize: '25px', 
+        margin:'10px',
+        fontWeight: 'bold'
+         }}
+      >
+        Buscador de Películas</Typography>
+     
+        <Input
           type="text"
           placeholder="Buscar películas..."
           value={textoBusqueda}
           onChange={manejarCambioDeBusqueda}
+          
+          sx={{
+            border: '1px solid',
+            borderColor: 'primary.main',
+            borderRadius: 1,
+            padding: '8px 12px',
+            margin:'5px',
+            width: '300px',
+            '&:focus': {
+            borderColor: 'primary.dark',
+            boxShadow: '0 0 0 2px rgba(25, 118, 210, 0.2)'
+            }
+          }}
         />
-       </div>
+      
 
 
     {loading && <p>Cargando...</p>}
@@ -38,27 +63,38 @@ function mostrarBusqueda() {
      <div className="movie-list" style={{ display: "flex", flexWrap: "wrap" }}>
         {!loading && pelis.length > 0 ? (
           pelis.map((peli) => (
-            <Card
-              key={peli.id}
-              style={{
-                width: "200px",
-                margin: "10px",
-                padding: "10px",
-                textAlign: "center",
-            }}
-            >
-            <h3>{peli.title}</h3>
-              {peli.poster_path ? (
-              <img
-                src={`https://image.tmdb.org/t/p/w500${peli.poster_path}`}
-                alt={peli.title}
-                width={150}
-               />
-              ) : (
-              <p>Sin imagen</p>
-              )}
-              <p>{peli.release_date?.slice(0, 4)}</p>
-           </Card>
+             <Card key={peli.id}
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  height: "200px",
+                  width: "90px",
+                  borderRadius: '0 !important',
+                  margin:'5px'
+
+                }}
+              >
+                {/* Imagen */}
+                <CardMedia
+                  component="img"
+                  image={`https://image.tmdb.org/t/p/w300${peli.poster_path}`}
+                  alt={peli.title}
+                  sx={{
+                    height: { xs: 220, sm: 280, md: 300 },
+                    objectFit: "cover",
+                    cursor: "pointer",
+                    borderRadius: '0 !important'
+                  }}
+                  onClick={() => navigate(`/pelicula/:${peli.id}`)}
+                 
+                />
+              
+
+                <Button onClick={() => handleFavoriteClick(peli)} color={isFavorite(peli.id) ? "error" : "default"}>
+              {isFavorite(peli.id) ? <FavoriteIcon /> : <FavoriteBorderIcon />}
+            </Button>
+
+              </Card>
           ))
       ) : (
          !loading && <p>No se encontraron películas</p>
